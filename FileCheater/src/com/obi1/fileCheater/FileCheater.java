ea637369a1a4dbf2d7fc26358383cd8ed4cbe920
jacfe02 <jacfe02@jacfe02.ca.com>
2016-04-05 23:52:18 -0300

    Send mail

--- FileCheater/src/com/obi1/fileCheater/FileCheater.java
+++ FileCheater/src/com/obi1/fileCheater/FileCheater.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -15,6 +16,7 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 import javax.swing.text.NumberFormatter;
 
@@ -22,8 +24,22 @@ public class FileCheater extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
 	private static FileCheater instance = null;
+	private static Runner runner;
+	
+	private SendMail sendMail;
+	private JProgressBar progressBar;
+	private JComboBox<String> cmbAction;
+	private JTextField txtFile;
+	private JFormattedTextField txtSize;
+	private JButton btnGo;
+	private JButton btnOpen;
 	
 	public FileCheater() throws Throwable {
+		runner = new Runner();
+		runner.start();
+
+		sendMail = new SendMail(this);
+		
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("File Cheater 1.0");
@@ -36,11 +52,11 @@ public class FileCheater extends JFrame {
 		lblSize.setBounds(12, 67, 118, 22);
 		
 		String[] actions = new String[]{"split", "merge"};
-		final JComboBox<String> cmbAction = new JComboBox<String>(actions);
+		cmbAction = new JComboBox<String>(actions);
 		cmbAction.setBounds(142, 13, 157, 22);
-		final JTextField txtFile = new JTextField();
+		txtFile = new JTextField();
 		txtFile.setLocation(142, 40);
-		final JFormattedTextField txtSize = new JFormattedTextField(new NumberFormatter());
+		txtSize = new JFormattedTextField(new NumberFormatter());
 		txtSize.setBounds(142, 67, 157, 22);
 		
 		txtFile.setSize(293, 22);
@@ -53,24 +69,29 @@ public class FileCheater extends JFrame {
 		getContentPane().add(lblSize);
 		getContentPane().add(txtSize);
 		
-		JButton btnGo = new JButton("Go!");
+		btnGo = new JButton("Go!");
 		btnGo.setBounds(374, 66, 97, 25);
 		getContentPane().add(btnGo);
 		
 		btnGo.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				runner.enable(false);
 				if (cmbAction.getSelectedItem().equals("split"))
-					split(txtFile.getText(), txtSize.getText().equals("") || Integer.parseInt(txtSize.getText()) <= 0 ? -1 
+					runner.splitAS(txtFile.getText(), txtSize.getText().equals("") || Integer.parseInt(txtSize.getText()) <= 0 ? -1 
 							: Integer.parseInt(txtSize.getText()));
 				else
-					merge(txtFile.getText());
+					runner.mergeAS(txtFile.getText());
 			}
 		});
 		
-		JButton btnOpen = new JButton("...");
+		btnOpen = new JButton("...");
 		btnOpen.setBounds(434, 39, 37, 23);
 		getContentPane().add(btnOpen);
+		
+		progressBar = new JProgressBar();
+		progressBar.setBounds(0, 102, 483, 14);
+		getContentPane().add(progressBar);
 		
 		btnOpen.addActionListener(new ActionListener() {
 			@Override
@@ -81,7 +102,7 @@ public class FileCheater extends JFrame {
 			}
 		});
 		
-		setSize(489, 135);
+		setSize(489, 148);
 		setLocation(Toolkit.getDefaultToolkit().getScreenSize().width/2-this.getSize().width/2, 
 				Toolkit.getDefaultToolkit().getScreenSize().height/2-this.getSize().height/2);
 		setVisible(true);
@@ -101,8 +122,8 @@ public class FileCheater extends JFrame {
 			String fileName = args[1];
 			String numBytes = (args.length == 3) ? args[2] : "-1";
 			
-			if (action.equals("split")) split(fileName, Integer.parseInt(numBytes));
-			else if (action.equals("merge")) merge(fileName);
+			if (action.equals("split")) runner.split(fileName, Integer.parseInt(numBytes));
+			else if (action.equals("merge")) runner.merge(fileName);
 			else printUsage();
 		}
 	}
@@ -116,110 +137,185 @@ public class FileCheater extends JFrame {
 		System.out.println(usage);
 	}
 	
-	private static void split(String fileName, int numBytes) {
-		FileOutputStream fos;
+	private class Runner extends Thread {
 		
-		try {
-			byte[] data = invertBytes(Files.readAllBytes(Paths.get(fileName)));
-			byte[] fileData;
-			byte[] lastFile;
-			int maxBytes = numBytes == -1 ? data.length : ((numBytes * 1024) * 1024);
-			int newFileSize = 0;
-			int numFile = 0;
-			
-			while (newFileSize < data.length) {
-				
-				fileData = new byte[maxBytes];
-				int i = 0;
-				for (; i < maxBytes && newFileSize < data.length; i++) {
-					fileData[i] = data[newFileSize];
-					newFileSize++;
-				}
-				
-				if (i != maxBytes) {
-					lastFile = new byte[i];
-					for (i = 0; i < lastFile.length; i++)
-						lastFile[i] = fileData[i];
-					fileData = lastFile;
-				}
-				
-				numFile++;
-				fos = new FileOutputStream(new File(fileName + ".ADELBS."+ numFile));
-				fos.write(fileData);
-				fos.close();
-			}
-			
-			showMessage("DONE! File splited in "+ numFile +" file(s).");
-		} 
-		catch (Exception x) {
-			x.printStackTrace();
-			showMessage(x.toString() +"\n\n"+ x.getMessage());
-		}
-	}
-	
-	private static void merge(String fileName) {
+		private String fileName;
+		private int numBytes;
+		private String currentAction = "";
 		
-		FileOutputStream fos;
-		File file = new File(fileName);
-		File fileRead;
-		
-		byte[] data;
-		byte[] aux;
-		byte[] finalFile = new byte[0];
-		int fileNum = 1;
-		int totalBytes = 0;
-		
-		if (!new File(file.getParent()).isDirectory() || !new File(fileName +".ADELBS.1").exists()) {
-			showMessage("File not found!");
-		}
-		else {
-			try {
-				fileRead = new File(fileName +".ADELBS."+ fileNum);
-				while (fileRead.exists()) {
-	
-					data = Files.readAllBytes(Paths.get(fileRead.getPath()));
-					totalBytes += data.length;
+		public void run() {
+			while (true) {
+				try {
+					if (currentAction.equals("split"))
+						split(fileName, numBytes);
+					else if (currentAction.equals("merge"))
+						merge(fileName);
 					
-					aux = finalFile;
-					finalFile = new byte[totalBytes];
+					enable(true);
+					Thread.sleep(100);
+				} 
+				catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+	    }
+
+		public void splitAS(String fileName, int numBytes) {
+			if (currentAction.equals("")) {
+				this.fileName = fileName;
+				this.numBytes = numBytes;
+				currentAction = "split";
+			}
+		}
+		
+		public void mergeAS(String fileName) {
+			if (currentAction.equals("")) {
+				this.fileName = fileName;
+				currentAction = "merge";
+			}
+		}
+		
+		public void split(String fileName, int numBytes) {
+			FileOutputStream fos;
+			
+			try {
+				byte[] data = invertBytes(Files.readAllBytes(Paths.get(fileName)));
+				byte[] fileData;
+				byte[] lastFile;
+				int maxBytes = numBytes == -1 ? data.length : ((numBytes * 1024) * 1024);
+				int newFileSize = 0;
+				int numFile = 0;
+				
+				ArrayList<File> files = new ArrayList<File>();
+				File newFile;
+				
+				while (newFileSize < data.length) {
+					
+					fileData = new byte[maxBytes];
 					int i = 0;
-					for (; i < aux.length; i++)
-						finalFile[i] = aux[i];
-					for (int j = 0; j < data.length; j++) {
-						finalFile[i] = data[j];
-						i++;
+					for (; i < maxBytes && newFileSize < data.length; i++) {
+						fileData[i] = data[newFileSize];
+						newFileSize++;
 					}
 					
-					fileNum++;
-					fileRead = new File(fileName +".ADELBS."+ fileNum);
+					if (i != maxBytes) {
+						lastFile = new byte[i];
+						for (i = 0; i < lastFile.length; i++)
+							lastFile[i] = fileData[i];
+						fileData = lastFile;
+					}
+					
+					numFile++;
+					newFile = new File(fileName + ".ADELBS."+ numFile);
+					fos = new FileOutputStream(newFile);
+					fos.write(fileData);
+					fos.close();
+					
+					files.add(newFile);
+					
+					updateStatus(new Long((newFileSize * 100L) / data.length).intValue());
 				}
 				
-				finalFile = invertBytes(finalFile);
-				fos = new FileOutputStream(file);
-				fos.write(finalFile);
-				fos.close();
-				
-				showMessage("DONE! "+ (fileNum - 1) +" file(s) merged.");
-			}
+				showMessage("DONE! File splited in "+ numFile +" file(s).");
+				if (instance != null) {
+					if (numFile <= 20 && numBytes <= 10 && JOptionPane.showConfirmDialog(instance, 
+							"Would you like to send the files via e-mail?", "Send e-mail", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+						sendMail.openDialog(files);
+					}
+				}
+			} 
 			catch (Exception x) {
 				x.printStackTrace();
 				showMessage(x.toString() +"\n\n"+ x.getMessage());
 			}
+			finally {
+				currentAction = "";
+			}
 		}
-	}
-	
-	private static byte[] invertBytes(byte[] data) {
-		byte[] newData;
-		newData = new byte[data.length];
-		for (int i = 0; i < data.length; i++)
-			newData[data.length - i -1] = data[i];
-		return newData;
-	}
-	
-	private static void showMessage(String msg) {
-		if (instance == null)
-			System.out.println(msg);
-		else
-			JOptionPane.showMessageDialog(instance, msg);
+		
+		public void merge(String fileName) {
+			
+			FileOutputStream fos;
+			File file = new File(fileName);
+			File fileRead;
+			
+			byte[] data;
+			byte[] aux;
+			byte[] finalFile = new byte[0];
+			int fileNum = 1;
+			int totalBytes = 0;
+			
+			if (!new File(file.getParent()).isDirectory() || !new File(fileName +".ADELBS.1").exists()) {
+				showMessage("File not found!");
+				currentAction = "";
+			}
+			else {
+				try {
+					fileRead = new File(fileName +".ADELBS."+ fileNum);
+					while (fileRead.exists()) {
+		
+						data = Files.readAllBytes(Paths.get(fileRead.getPath()));
+						totalBytes += data.length;
+						
+						aux = finalFile;
+						finalFile = new byte[totalBytes];
+						int i = 0;
+						for (; i < aux.length; i++)
+							finalFile[i] = aux[i];
+						for (int j = 0; j < data.length; j++) {
+							finalFile[i] = data[j];
+							i++;
+						}
+						
+						fileNum++;
+						fileRead = new File(fileName +".ADELBS."+ fileNum);
+					}
+					
+					finalFile = invertBytes(finalFile);
+					fos = new FileOutputStream(file);
+					fos.write(finalFile);
+					fos.close();
+					
+					showMessage("DONE! "+ (fileNum - 1) +" file(s) merged.");
+				}
+				catch (Exception x) {
+					x.printStackTrace();
+					showMessage(x.toString() +"\n\n"+ x.getMessage());
+				}
+				finally {
+					currentAction = "";
+				}
+			}
+		}
+		
+		private byte[] invertBytes(byte[] data) {
+			byte[] newData;
+			newData = new byte[data.length];
+			for (int i = 0; i < data.length; i++)
+				newData[data.length - i -1] = data[i];
+			return newData;
+		}
+		
+		private void showMessage(String msg) {
+			if (instance == null)
+				System.out.println(msg);
+			else
+				JOptionPane.showMessageDialog(instance, msg);
+		}
+
+		private void updateStatus(int value) {
+			if (progressBar != null)
+				progressBar.setValue(value);
+		}
+		
+		private void enable(boolean value) {
+			if (progressBar != null) {
+				cmbAction.setEnabled(value);
+				txtFile.setEnabled(value);
+				txtSize.setEnabled(value);
+				btnGo.setEnabled(value);
+				btnOpen.setEnabled(value);
+			}
+		}
 	}
 }
---
+++ FileCheater/src/com/obi1/fileCheater/SendMail.java
@@ -0,0 +1,293 @@
+package com.obi1.fileCheater;
+
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JProgressBar;
+
+public class SendMail extends JDialog {
+	
+	private static final long serialVersionUID = 1L;
+
+	private static Sender sender;
+	
+	private JProgressBar progressBar;
+	private JTextField txtTo;
+	private JTextField txtFrom;
+	private JTextField txtUser;
+	private JPasswordField txtPwd;
+	private JTextField txtHost;
+	private JTextField txtPort;
+	private JTextPane txtMessage;
+	private JButton btnSend;
+	
+	private ArrayList<File> files;
+	
+	public SendMail(FileCheater parent) {
+		super(parent, "Send e-mail", true);
+		
+		sender = new Sender();
+		sender.start();
+		
+		setResizable(false);
+		getContentPane().setLayout(null);
+		
+		JLabel lblTo = new JLabel("To:");
+		lblTo.setBounds(12, 13, 68, 16);
+		getContentPane().add(lblTo);
+		
+		JLabel lblFrom = new JLabel("From:");
+		lblFrom.setBounds(12, 42, 68, 16);
+		getContentPane().add(lblFrom);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setBounds(12, 71, 68, 16);
+		getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(12, 100, 68, 16);
+		getContentPane().add(lblPassword);
+		
+		JLabel lblHost = new JLabel("Host:");
+		lblHost.setBounds(12, 129, 68, 16);
+		getContentPane().add(lblHost);
+		
+		JLabel lblPort = new JLabel("Port:");
+		lblPort.setBounds(12, 158, 68, 16);
+		getContentPane().add(lblPort);
+		
+		txtTo = new JTextField();
+		txtTo.setBounds(92, 13, 247, 22);
+		getContentPane().add(txtTo);
+		txtTo.setColumns(10);
+		
+		txtFrom = new JTextField();
+		txtFrom.setBounds(92, 39, 247, 22);
+		getContentPane().add(txtFrom);
+		txtFrom.setColumns(10);
+		
+		txtUser = new JTextField();
+		txtUser.setBounds(92, 68, 247, 22);
+		getContentPane().add(txtUser);
+		txtUser.setColumns(10);
+		
+		txtPwd = new JPasswordField();
+		txtPwd.setBounds(92, 97, 247, 22);
+		getContentPane().add(txtPwd);
+		txtPwd.setColumns(10);
+		
+		txtHost = new JTextField();
+		txtHost.setText("outlook.office365.com");
+		txtHost.setBounds(92, 126, 247, 22);
+		getContentPane().add(txtHost);
+		txtHost.setColumns(10);
+		
+		txtPort = new JTextField();
+		txtPort.setText("587");
+		txtPort.setBounds(92, 155, 116, 22);
+		getContentPane().add(txtPort);
+		txtPort.setColumns(10);
+		
+		btnSend = new JButton("Send");
+		btnSend.setBounds(242, 396, 97, 25);
+		getContentPane().add(btnSend);
+		
+		JLabel lblMessage = new JLabel("Message:");
+		lblMessage.setBounds(12, 187, 68, 16);
+		getContentPane().add(lblMessage);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(92, 190, 250, 199);
+		getContentPane().add(scrollPane);
+		
+		txtMessage = new JTextPane();
+		txtMessage.setText("You are gonna to receive several e-mails with a splited file. To merge these files, "+
+							"follow the instructions below:\r\n\r\n1) Rename the FileCheater.pdf to FileCheater.jar\r\n"+
+							"2) Run the FileCheater.jar\r\n3) Select the \"merge\" option\r\n4) Inform the name of the "+
+							"splited file (without the ADELBS.n extension)\r\n");
+		scrollPane.setViewportView(txtMessage);
+		
+		progressBar = new JProgressBar();
+		progressBar.setBounds(0, 431, 355, 14);
+		getContentPane().add(progressBar);
+
+		setSize(361, 477);
+		
+		btnSend.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (!isSomeEmpty()) {
+					sender.enable(false);
+					sender.sendMail();
+				}
+			}
+		});
+	}
+	
+	public void openDialog(ArrayList<File> files) {
+		this.files = files;
+		
+		setLocation(Toolkit.getDefaultToolkit().getScreenSize().width/2-this.getSize().width/2, 
+				Toolkit.getDefaultToolkit().getScreenSize().height/2-this.getSize().height/2);
+
+		
+		setVisible(true);
+	}
+	
+	private boolean isSomeEmpty() {
+		return txtTo.getText().trim().equals("") || txtFrom.getText().trim().equals("") || txtUser.getText().trim().equals("") ||
+				new String(txtPwd.getPassword()).trim().equals("") || txtHost.getText().trim().equals("") || 
+				txtPort.getText().trim().equals("") || txtMessage.getText().trim().equals("");
+	}
+	
+	private class Sender extends Thread {
+
+		private boolean send = false;
+		
+		public void run() {
+		
+			while (true) {
+				try {
+					if (send) {
+						sendMail(txtTo.getText(), txtFrom.getText(), txtUser.getText(), new String(txtPwd.getPassword()), 
+								txtHost.getText(), txtPort.getText(), "Splited File: instruction", txtMessage.getText(), 
+								SendMail.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+						
+						for (int i = 0; i < files.size(); i++) {
+							sendMail(txtTo.getText(), txtFrom.getText(), txtUser.getText(), new String(txtPwd.getPassword()), 
+									txtHost.getText(), txtPort.getText(), "Splited File: "+ (i + 1) +" of "+ files.size(), 
+									"", files.get(i).getPath());
+							
+							updateStatus(new Long((i * 100L) / files.size()).intValue());
+						}
+						
+						updateStatus(100);
+						JOptionPane.showMessageDialog(SendMail.this, "E-mail sent!");
+
+						enable(true);
+						send = false;
+						SendMail.this.setVisible(false);
+					}
+					
+					Thread.sleep(100);
+				} 
+				catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		private void sendMail() {
+			send = true;
+		}
+		
+		/**
+		 * from = felipe.jacob@ca.com
+		 * username = jacfe02@ca.com
+		 * host = outlook.office365.com
+		 * port = 587
+		 */
+		private void sendMail(String to, String from, final String username, final String password, String host, String port, 
+				String title, String msg, String filename) {
+
+			Properties props = new Properties();
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", host);
+			props.put("mail.smtp.port", port);
+
+			// Get the Session object.
+			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+					return new javax.mail.PasswordAuthentication(username, password);
+				}
+			});
+
+			try {
+				// Create a default MimeMessage object.
+				Message message = new MimeMessage(session);
+
+				// Set From: header field of the header.
+				message.setFrom(new InternetAddress(from));
+
+				// Set To: header field of the header.
+				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+
+				// Set Subject: header field
+				message.setSubject(title);
+
+				// Create the message part
+				BodyPart messageBodyPart = new MimeBodyPart();
+
+				// Now set the actual message
+				messageBodyPart.setText(msg);
+
+				// Create a multipar message
+				Multipart multipart = new MimeMultipart();
+
+				// Set text message part
+				multipart.addBodyPart(messageBodyPart);
+
+				// Part two is attachment
+				messageBodyPart = new MimeBodyPart();
+				DataSource source = new FileDataSource(filename);
+				messageBodyPart.setDataHandler(new DataHandler(source));
+				messageBodyPart.setFileName(filename);
+				multipart.addBodyPart(messageBodyPart);
+
+				// Send the complete message parts
+				message.setContent(multipart);
+
+				// Send message
+				Transport.send(message);
+
+				System.out.println("Sent message ("+ title +") successfully....");
+			} 
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		private void updateStatus(int value) {
+			if (progressBar != null)
+				progressBar.setValue(value);
+		}
+
+		private void enable(boolean value) {
+			if (progressBar != null) {
+				txtTo.setEnabled(value);
+				txtFrom.setEnabled(value);
+				txtUser.setEnabled(value);
+				txtPwd.setEnabled(value);
+				txtHost.setEnabled(value);
+				txtPort.setEnabled(value);
+				txtMessage.setEnabled(value);
+				btnSend.setEnabled(value);
+			}
+		}
+	}
+}
